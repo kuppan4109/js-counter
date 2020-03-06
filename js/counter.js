57dccf5540587386824a7e5f207a278d0bced948
kuppan <42442509+kuppan4109@users.noreply.github.com>
2020-03-06 15:55:26 +0530

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,9 +1,13 @@
 var c=0;
-
+document.getElementById('txt').value=c;
 function startCount()
 {
-  document.getElementById('txt').value=c;
+  c=parseInt(document.getElementById('txt').value)
+  if(c<10 && c>=0)
+  {
   c=c+1;
+  }
+  document.getElementById('txt').value=c;
 }
 
 function stopCount()
